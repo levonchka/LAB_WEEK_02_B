5656262abc5328de1cf0cdb7371e3b62f420a066
Jeremy Dominic <jeremydag17@gmail.com>
2025-09-01 14:11:14 +0700

    Commit No. 2 –Result activity

---
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -0,0 +1,35 @@
+package com.example.lab_week_02_b
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ResultActivity : AppCompatActivity() {
+
+    companion object {
+        private const val COLOR_KEY = "COLOR_KEY"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        intent?.let { receivedIntent ->
+            val colorCode = receivedIntent.getStringExtra(COLOR_KEY)
+
+            val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
+            val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
+
+            if (!colorCode.isNullOrEmpty()) {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+
+                resultMessage.text = getString(
+                    R.string.color_code_result_message,
+                    colorCode.uppercase()
+                )
+            }
+        }
+    }
+}
